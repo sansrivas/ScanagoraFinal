969f023b9128110e227bebe1c8c0e204faa7c740
Alejandro Bautista <abautista@scanagora.com>
2015-10-13 11:56:04 -0500

    First commit

---
+++ client/Router.js
@@ -0,0 +1,21 @@
+Router.route('/', function () {
+    this.render('home');
+});
+Router.route('/home', function () {
+    this.render('home');
+});
+Router.route('/about', function () {
+    this.render('about');
+});
+Router.route('/contact', function () {
+    this.render('contact');
+});
+Router.route('/marketplace', function () {
+    this.render('marketplace');
+});
+Router.route('/pricing', function () {
+    this.render('pricing');
+});
+Router.route('/registrationpage', function () {
+    this.render('registrationpage');
+});
